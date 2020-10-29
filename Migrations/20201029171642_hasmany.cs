aba422bb6991f608c1f2e916268e838540f199e9
simotoytari <simotoytari@gmail.com>
2020-10-29 19:19:16 +0200

    Uusi migraatio WithMany lisätty

---
+++ Migrations/20201029171642_hasmany.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Picture_Catalog.Migrations
+{
+    public partial class hasmany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
